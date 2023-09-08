dcfb7362acf4b41ae4ccee0fcb807ac52a7fc2e3
Lucas Cavalcante Joca Eugênio <lucasjoca27@gmail.com>
2023-09-08 18:14:05 -0300

    aula12ex 2

--- aula12ex copy/script.js
+++ aula12ex copy/script.js
@@ -1,21 +1,49 @@
-function carregar() {
-    var msg = document.querySelector(`div#msg`)
-    var img = document.querySelector(`img#fots`)
+function verificar() {
     var data = new Date
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    ano = data.getFullYear()
+    var fano = document.querySelector(`input#txtano`)
+    var res = document.querySelector(`div#res`)
 
-    if (hora >= 0 && hora <12) {
-        //bom dia
-        img.src = 'imagens/fotomanha.png'
-        document.body.style.background = `#CEAE8A`
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-        img.src = 'imagens/fototarde.png'
-        document.body.style.background = `#975020`
+    if (fano.value.length == 0 || fano.value > ano) {
+        alert(`ERRO! Verifique os dados e tente novamente!`)
     } else {
-        //boa noite
-        img.src = 'imagens/fotonoite.png'
-        document.body.style.background = `#574571`
+        var fsex = document.getElementsByName(`radsex`)
+        var idade = ano - Number(fano.value)
+        var genero = ``
+        var img = document.createElement(`img`)
+        img.setAttribute(`id`,`fots`)
+        if (fsex[0].checked) {
+            genero = `homem`
+            if (idade < 10) {
+                //criança
+                img.setAttribute(`src`,`imagens/bebe-m.jpg`)
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute(`src`,`imagens/jovem-m.jpg`)
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute(`src`,`imagens/adulto-m.jpg`)
+            } else {
+                //idoso
+                img.setAttribute(`src`,`imagens/idoso-m.jpg`)
+            }
+        } else {
+            genero = `mulher`
+            if (idade < 10) {
+                //criança
+                img.setAttribute(`src`,`imagens/bebe-f.jpg`)
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute(`src`,`imagens/jovem-f.jpg`)
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute(`src`,`imagens/adulta-f.jpg`)
+            } else {
+                //idoso
+                img.setAttribute(`src`,`imagens/idosa-f.jpg`)
+            }
+        }
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
